48e7187e0367258d184470848be4a49b551ba752
dhayithri <dhayithrim@gmail.com>
2016-12-01 14:07:58 +0530

    backend

---
+++ src/main/java/com/niit/collab/dao/UsersDAO.java
@@ -0,0 +1,15 @@
+package com.niit.collab.dao;
+
+import java.util.List;
+
+import com.niit.collab.model.Users;
+
+public interface UsersDAO {
+
+	public boolean saveOrUpdate(Users users);
+	public boolean delete(Users users);
+	public List<Users>list();
+	public List<Users> getuser(int id);
+	public Users logout(int id);
+	public Users authuser(String username,String password);
+}
